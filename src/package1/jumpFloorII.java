790683455026537aabf8b0510bf14e97f3902ee5
citrusli <15927162537@163.com>
2019-04-03 18:27:37 +0800

    Initial commit

--- src/package1/jumpFloorII.java
+++ src/package1/jumpFloorII.java
@@ -6,7 +6,40 @@ public class jumpFloorII {
      */
 
     //自己的代码
+    /*
+     * 已经看不懂了
+     */
     public int JumpFloorII1(int target) {
-        
+        if(target == 1 || target == 0 || target == 2)
+            return target;
+        int type = 0;
+        for(int i = 1; i <= target; i++){
+
+            type += JumpFloorII1(target-i);
+        }
+        type++;
+        return type;
+    }
+
+    //推荐代码
+    /*
+     * n = 1时，f(1)=1
+     * n = 2时，f(2)= f(2-1) + f(2-2)
+     * n = 3时，f(3)= f(3-1) + f(3-2) + f(3-3)
+     * ...
+     * f(n) = f(n-1) + f(n-2) + f(n-3) + ... + f(n-n)
+     * 化简表达式可以得到f(n) = 2*f(n-1)
+     *         1  (n=0)
+     * f(n) =  1  (n=1)
+     *         2*f(n-1)  (n>1)
+     */
+    public int JumpFloorII2(int target) {
+        if (target <= 0) {
+            return -1;
+        } else if (target == 1) {
+            return 1;
+        } else {
+            return 2 * JumpFloorII2(target - 1);
+        }
     }
 }
