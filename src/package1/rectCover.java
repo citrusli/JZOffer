ab4ed04c686f7c18fb93f527c03725e68143f5f2
citrusli <15927162537@163.com>
2019-04-03 18:43:26 +0800

    Initial commit

---
+++ src/package1/rectCover.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class rectCover {
+    /*
+    我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+    请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+     */
+
+    //自己的代码
+    /*
+     * 与跳台阶的问题相似
+     */
+    public int RectCover(int target) {
+        if(target == 0 || target == 1 || target == 2)
+            return target;
+        return RectCover(target-1)+RectCover(target-2);
+    }
+}
